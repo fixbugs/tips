b11c3a420f7c92a0d775760ced2ceb68e15c3aa4
fuxin_home <goitt0519@gmail.com>
2016-01-09 00:28:05 +0800

    add news sql test example

---
+++ application/models/News_model.php
@@ -0,0 +1,22 @@
+<?php
+class News_model extends CI_Model {
+
+    public function __construct()
+    {
+        $this->load->database();
+        var_dump($this->db);
+        die("Aaa");
+    }
+
+    public function get_news($slug = FALSE)
+    {
+        if ($slug === FALSE)
+        {
+            $query = $this->db->get('news');
+            return $query->result_array();
+        }
+
+        $query = $this->db->get_where('news', array('slug' => $slug));
+        return $query->row_array();
+    }
+}
