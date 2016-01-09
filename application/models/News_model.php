3c98d3d9fd218d99c88785802b70cb6c2e552ec6
fuxin_home <goitt0519@gmail.com>
2016-01-09 20:44:33 +0800

    test mysql connect ok

--- application/models/News_model.php
+++ application/models/News_model.php
@@ -4,8 +4,6 @@ class News_model extends CI_Model {
     public function __construct()
     {
         $this->load->database();
-        var_dump($this->db);
-        die("Aaa");
     }
 
     public function get_news($slug = FALSE)
