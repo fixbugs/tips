bb5ab695f2bc78bb1eda1b97a5bd3510e1ec9a1b
fuxin_home <goitt0519@gmail.com>
2016-01-10 23:47:10 +0800

    add main table create for tips

--- script/sql.sql
+++ script/sql.sql
@@ -1,15 +1,68 @@
 CREATE TABLE `user` (
-`id` BIGINT(20) NOT NULL,
+`user_id` BIGINT(20) NOT NULL,
 `username` CHAR(32) NOT NULL COMMENT '用户名',
 `password` CHAR(32) NOT NULL COMMENT '密码',
-`truename CHAR(40) NOT NULL DEFAULT '' COMMENT '真实姓名',
-`email` VARCHAR(60) NOT NULL DEFAULT '' COMMENT '邮箱',
+`truename` CHAR(40) NOT NULL DEFAULT '' COMMENT '真实姓名',
+`email` VARCHAR(80) NOT NULL DEFAULT '' COMMENT '邮箱',
 `create_time` INT(11) NOT NULL DEFAULT '0',
 `update_time` INT(11) NOT NULL DEFAULT '0',
-PRIMARY KEY (`id`),
-KEY `username` (`username`),
-KEY `email` (`email`),
+PRIMARY KEY (`user_id`),
 KEY `create_update_time` (`create_time`,`update_time`),
 KEY `user_name_password` (`username`,`password`),
 UNIQUE KEY `username` (`username`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8 COMMENT='用户表';
+
+CREATE TABLE `log` (
+`id` BIGINT(20) NOT NULL,
+`lever` CHAR(10) NOT NULL COMMENT '等级',
+`message` CHAR(80) NOT NULL COMMENT '信息内容',
+`user_id` BIGINT(20) NOT NULL DEFAULT '0' COMMENT '用户ID',
+`create_time` INT(11) NOT NULL DEFAULT '0',
+PRIMARY KEY (`id`),
+KEY `lever` (`lever`),
+KEY `create_time` (`create_time`),
+KEY `user_id` (`user_id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8 COMMENT='日志表';
+
+CREATE TABLE `api_log` (
+`id` BIGINT(20) NOT NULL,
+`lever` CHAR(10) NOT NULL COMMENT '等级',
+`error_num` INT(4) NOT NULl DEFAULT '0' COMMENT '错误状态码',
+`message` CHAR(80) NOT NULL COMMENT '信息内容',
+`user_id` BIGINT(20) NOT NULL DEFAULT '0' COMMENT '用户ID',
+`create_time` INT(11) NOT NULL DEFAULT '0',
+PRIMARY KEY (`id`),
+KEY `lever` (`lever`),
+KEY `create_time` (`create_time`),
+KEY `user_id` (`user_id`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8 COMMENT='API日志表';
+
+CREATE TABLE `trace` (
+`id` BIGINT(20) NOT NULL,
+`lever` CHAR(10) NOT NULL COMMENT '等级',
+`message` CHAR(80) NOT NULL COMMENT '信息内容',
+`user_id` BIGINT(20) NOT NULL DEFAULT '0' COMMENT '用户ID',
+`obj_id` BIGINT(20) NOT NULL DEFAULT '0' COMMENT '对象ID',
+`obj_type` CHAR(20) NOT NULL DEFAULT 'other' COMMENT '对象操作类型,add,edit,deleted,other',
+`create_time` INT(11) NOT NULL DEFAULT '0',
+PRIMARY KEY (`id`),
+KEY `lever` (`lever`),
+KEY `create_time` (`create_time`),
+KEY `user_id` (`user_id`),
+KEY `obj_id_type` (`obj_id`,`obj_type`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8 COMMENT='操作日志表';
+
+CREATE TABLE `tips` (
+`tips_id` BIGINT(20) NOT NULL,
+`parent_id` BIGINT(20) NOT NULL DEFAULT '0' COMMENT '父ID',
+`tips_message` TEXT NOT NULL COMMENT '信息内容',
+`user_id` BIGINT(20) NOT NULL COMMENT '用户ID',
+`create_time` INT(11) NOT NULL DEFAULT '0',
+`edit_time` INT(11) NOT NULL DEFAULT '0',
+`status` CHAR(20) NOT NULL DEFAULT 'nonstart' COMMENT '任务状态,nonstart->start->executions->finished',
+PRIMARY KEY (`tips_id`),
+KEY `tips_paranet_id` (`tips_id`,`parent_id`),
+KEY `tips_user_status` (`tips_id`,`user_id`,`status`),
+KEY `create_time` (`create_time`),
+KEY `edit_time` (`edit_time`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8 COMMENT='提示任务表';
