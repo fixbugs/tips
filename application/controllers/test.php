8835416c3ff49269c2cdaa07151cab3c75d965b4
fuxin_home <goitt0519@gmail.com>
2016-01-14 00:26:28 +0800

    test git hook

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -13,7 +13,6 @@ class test extends CI_Controller{
         $data['title'] = ucfirst($page); // Capitalize the first letter
         $this->load->view('templates/header', $data);
         $this->load->view('templates/footer', $data);
-
     }
 
     public function testuser(){
