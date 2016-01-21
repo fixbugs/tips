0127de88e2e8d14e7a958aafa658e2d1d74f76ed
fuxin_wk_pc <goitt0519@gmail.com>
2016-01-21 19:47:11 +0800

    add tips controller

---
+++ application/controllers/Tips.php
@@ -0,0 +1,49 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Tips extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url_helper');
+    }
+
+    public function index()
+    {
+        if($this->isPost()){
+            $params = $this->input->post();
+            var_dump($params);
+        }else{
+            $params = $this->input->get();
+            var_dump($params);
+        }
+     }
+
+    /**
+     * 添加提醒方法
+     * @return json
+     */
+    public function add()
+    {
+        $params = $this->isPost()? $this->input->post():$this->input->get();
+        /*$ret = $this->user_model->addUser($params);
+        if($ret){
+            $result = array_for_result(true,$this->user_model->getModelError());
+        }else{
+            $result = array_for_result(flase,$this->user_model->getModelError());
+        }
+        $this->renderJson($result);*/
+    }
+
+    /**
+     * 获取提醒
+     * @return json
+     */
+    public function listall(){
+        /*$data = $this->user_model->findAll();
+        $result = array_for_list($data,array(),true,'success');
+        $this->renderJson($result);*/
+    }
+
+}
