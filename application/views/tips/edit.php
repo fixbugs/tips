83d4fc15a43160af5fa7cf241a24b936318872b5
fuxin_home <goitt0519@gmail.com>
2016-02-14 19:17:49 +0800

    com edit func for tips

--- application/views/tips/edit.php
+++ application/views/tips/edit.php
@@ -1,7 +1,10 @@
 <form action='<?php if($action == "edit"){ echo '/index.php/tips/edit';}else{ echo '/index.php/tips/add';} ?>' method='post'>
+<?php if($data['tips_id']){ ?>
+<input type='hiddena' name='tips_id' value='<?php if($data["tips_id"]){ echo $data["tips_id"];}?>'>
+<?php } ?>
 <ul>
-     <li><input type='text' name='message' value=''></li>
-     <li><input type='text' name='parent_id' value=''></li>
+     <li><input type='text' name='message' value='<?php if($data["tips_message"]){ echo $data["tips_message"];}?>'></li>
+     <li><input type='text' name='parent_id' value='<?php if($data["parent_id"]){ echo $data["parent_id"];}?>'></li>
      <li><button type='submit' value='Submit'>Submit</button></li>
      <li><button type='reset' value='Reset'>Reset</button></li>
 </ul>
