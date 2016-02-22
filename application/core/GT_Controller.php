51f07f0ebd302efc637822e8cee7ed4d08e17310
fuxin_home <goitt0519@gmail.com>
2016-02-22 21:40:49 +0800

    remove pub controller function into controller

--- application/core/GT_Controller.php
+++ application/core/GT_Controller.php
@@ -2,12 +2,117 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+/**
+ * 公共处理Controller
+ */
 class GT_Contorller extends CI_Controller{
-
-    private static $instance;
 
     public function __construct(){
         parent::__construct();
+    }
+
+    /**
+     * init controller more thing
+     * @return null
+     */
+    public function _initContorller(){
+        $this->_loginCheck();
+        $this->_initConst();
+    }
+
+    /**
+     * init const var
+     * @return null
+     */
+    public function _initConst(){
+        define('USER_ID', $this->_user['user_id']);
+        define('USER_NAME', $this->_user['username']);
+    }
+
+    /**
+     * check user login and set session
+     * @return null
+     */
+    public function _loginCheck(){
+        if(!$this->isLogin()){
+            $this->_gotoLogin();
+        }else{
+            //get user info
+            $this->load->model('user_model');
+            $this->_user = $this->user_model->findByUsername('admin');
+        }
+    }
+
+    /**
+     * check is login
+     * @return boolean [description]
+     */
+    public function isLogin(){
+        return true;
+        //check user login cookie
+        //cookie rule
+        //uid lt ssokey
+        if(!$_COOKIE['admin_permit']){
+            return false;
+        }else{
+
+            return $this->checkPermit( $_COOKIE['admin_permit']);
+        }
+
+        return true;
+    }
+
+    /**
+     * check user permit by ssokey and cookie
+     * @param string $sso_key 统一分配的ssokey
+     * @param string $admin_permit 权限cookie值
+     * @return bool
+     */
+    public function checkPermit($sso_key, $admin_permit=''){
+        $permit = json_decode($admin_permit, true);
+        $pro_key = md5($sso_key.'_'.$permit['lt'].'_'.$permit['uid']);
+        $key = substr($pro_key,2,1).substr($pro_key,7,1).substr($pro_key,17,1).substr($pro_key,25,1).substr($pro_key,31,1);
+        $key_pos = strpos($permit['permit'], $key);
+        if($key_pos !== false && ($key_pos % 5) == 0 && (time()-$permit['lt']) <72000 ){
+            if(!empty($admin_permit)){
+                $_COOKIE['admin_permit'] = $admin_permit;
+                setcookie('admin_permit', $admin_permit, time()+72000, '/', getDomain($_SERVER['HTTP_HOST']));
+            }
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * redirect to error page
+     * @return null redirect
+     */
+    public function _gotoError($message,$return_url=''){
+        if ($return_url == '') {
+            $return_url = ! empty ( $_SERVER ['HTTP_REFERER'] ) ? $_SERVER ['HTTP_REFERER'] : '/';
+        }
+        if (_IS_AJAX_) {
+            $result = array_for_result ( false, $message, array(), $return_url );
+            echo $this->renderJson($result);
+            exit ();
+        } else {
+            $this->assign ( 'message', $message );
+            $this->assign ( 'return_url', $return_url );
+            //todo view to show message v
+            //echo $this->render ( 'message' );
+            exit ($message);
+        }
+    }
+
+    /**
+     * redirect to login page
+     * @return null redirect
+     */
+    public function _gotoLogin($return_url=''){
+        $total_login_url = 'http://'.$_SERVER['HTTP_HOST'].'/index.php/login/index';
+        $return_url = $return_url==''? urlencode("http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']) : urlencode($return_url);
+        header("Location:" . $total_login_url . "?returnurl=" . $return_url);
+        exit();
     }
 
 }
