570e68e82f02d07728e042973ab94504d8199f0b
fuxin_home <goitt0519@gmail.com>
2016-02-22 21:54:43 +0800

    remove unused code

--- system/core/Controller.php
+++ system/core/Controller.php
@@ -77,7 +77,6 @@ class CI_Controller {
 
 		$this->load =& load_class('Loader', 'core');
 		$this->load->initialize();
-        //$this->_initContorller();
 		log_message('info', 'Controller Class Initialized');
 	}
 
