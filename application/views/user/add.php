f46ea7c54fac64da55bb5e380e9ad941f40e7bf6
fuxin_wk_pc <goitt0519@gmail.com>
2016-02-26 20:43:45 +0800

    fix add

--- application/views/user/add.php
+++ application/views/user/add.php
@@ -1,13 +1,10 @@
 <form action='<?php if($action == "edit"){ echo '/index.php/user/edit';}else{ echo '/index.php/user/add';} ?>' method='post'>
-<?php if(isset($data) ){ ?>
-<input type='hidden' name='user_id' value='<?php echo $data["user_id"]; ?>'>
-<?php } ?>
 <ul>
-     <li><input type='text' name='username' value='<?php if(isset($data) ){ echo $data["username"];} ?>'></li>
-     <li><input type='password' name='password' value='<?php if(isset($data) ){ echo $data["password"];} ?>'></li>
+     <li><input type='text' name='username' value=''></li>
+     <li><input type='password' name='password' value=''></li>
      <li><input type='password' name='re_password' value=''></li>
-     <li><input type='text' name='truename' value='<?php if(isset($data) ){ echo $data["truename"];} ?>'></li>
-     <li><input type='password' name='email' value='<?php if(isset($data) ){ echo $data["email"];} ?>'></li>
+     <li><input type='text' name='truename' value=''></li>
+     <li><input type='password' name='email' value=''></li>
      <li><button type='submit' value='Submit'>Submit</button></li>
      <li><button type='reset' value='Reset'>Reset</button></li>
 </ul>
