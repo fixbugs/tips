0aba339042604472bcdf7238b4b7c2d27c7f590e
fuxin_home <goitt0519@gmail.com>
2016-03-10 23:12:34 +0800

    add pub func ismobile for count

--- application/helpers/GT_common_helper.php
+++ application/helpers/GT_common_helper.php
@@ -703,7 +703,69 @@ function setCountInfo(){
     $data['user_ip'] = getClientIp();
     $data['create_time'] = isset($_SERVER['REQUEST_TIME']) ? $_SERVER['REQUEST_TIME']:time();
     $data['http_info'] = json_encode($_SERVER);
+
+$other_data['pre_page'] = $data['refer'];
+$other_data['now_page'] = $data['url'];
+$other_data['domain'] = getDomain($data['url']);
+$other_data['city'] = '';
+$other_data['equipment'] = '';//ipad iphone
+$other_data['equipment_type'] = '';//pc mobile ipad iphone 
+var_dump($other_data);
+die("ddd");
     $CI = & get_instance();
     $system_count_model = $CI->load->model('system_count_model');
     $CI->system_count_model->insert($data);
+}
+
+/**
+* @description 判断是否是移动设备访问
+* @return bool
+*/
+function isMobile ()
+{
+    $_SERVER['ALL_HTTP'] = isset( $_SERVER['ALL_HTTP'] ) ? $_SERVER['ALL_HTTP'] : '';
+    $mobile_browser      = '0';
+    if ( preg_match( '/(up.browser|up.link|mmp|symbian|smartphone|midp|wap|phone|iphone|ipad|ipod|android|xoom)/i' , strtolower( $_SERVER['HTTP_USER_AGENT'] ) ) ){
+        $mobile_browser++;
+    }
+    if ( (isset( $_SERVER['HTTP_ACCEPT'] )) and (strpos( strtolower( $_SERVER['HTTP_ACCEPT'] ) , 'application/vnd.wap.xhtml+xml' ) !== false) ){
+        $mobile_browser++;
+    }
+    if ( isset( $_SERVER['HTTP_X_WAP_PROFILE'] ) ){
+        $mobile_browser++;
+    }
+    if ( isset( $_SERVER['HTTP_PROFILE'] ) ){
+        $mobile_browser++;
+    }
+    $mobile_ua           = strtolower( substr( $_SERVER['HTTP_USER_AGENT'] , 0 , 4 ) );
+    $mobile_agents       = array(
+        'w3c ' , 'acs-' , 'alav' , 'alca' , 'amoi' , 'audi' , 'avan' , 'benq' , 'bird' , 'blac' ,
+        'blaz' , 'brew' , 'cell' , 'cldc' , 'cmd-' , 'dang' , 'doco' , 'eric' , 'hipt' , 'inno' ,
+        'ipaq' , 'java' , 'jigs' , 'kddi' , 'keji' , 'leno' , 'lg-c' , 'lg-d' , 'lg-g' , 'lge-' ,
+        'maui' , 'maxo' , 'midp' , 'mits' , 'mmef' , 'mobi' , 'mot-' , 'moto' , 'mwbp' , 'nec-' ,
+        'newt' , 'noki' , 'oper' , 'palm' , 'pana' , 'pant' , 'phil' , 'play' , 'port' , 'prox' ,
+        'qwap' , 'sage' , 'sams' , 'sany' , 'sch-' , 'sec-' , 'send' , 'seri' , 'sgh-' , 'shar' ,
+        'sie-' , 'siem' , 'smal' , 'smar' , 'sony' , 'sph-' , 'symb' , 't-mo' , 'teli' , 'tim-' ,
+        'tosh' , 'tsm-' , 'upg1' , 'upsi' , 'vk-v' , 'voda' , 'wap-' , 'wapa' , 'wapi' , 'wapp' ,
+        'wapr' , 'webc' , 'winw' , 'winw' , 'xda' , 'xda-'
+        );
+    if ( in_array( $mobile_ua , $mobile_agents ) ){
+        $mobile_browser++;
+    }
+    if ( strpos( strtolower( $_SERVER['ALL_HTTP'] ) , 'operamini' ) !== false ){
+        $mobile_browser++;
+    }
+    // Pre-final check to reset everything if the user is on Windows
+    if ( strpos( strtolower( $_SERVER['HTTP_USER_AGENT'] ) , 'windows' ) !== false ){
+        $mobile_browser      = 0;
+    }
+    // But WP7 is also Windows, with a slightly different characteristic
+    if ( strpos( strtolower( $_SERVER['HTTP_USER_AGENT'] ) , 'windows phone' ) !== false ){
+        $mobile_browser++;
+    }
+    if ( $mobile_browser > 0 ){
+        return true;
+    }else{
+        return false;
+    }
 }
