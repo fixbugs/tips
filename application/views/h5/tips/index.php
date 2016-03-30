32bf6388e26f83a5ddbfd5e7c47507b3eee93fd4
fuxin_home <goitt0519@gmail.com>
2016-03-30 22:44:28 +0800

    format h5 code for tips index page

--- application/views/h5/tips/index.php
+++ application/views/h5/tips/index.php
@@ -1,3 +1,40 @@
-<div>
-  <h3 class='tx'>测试内容</h3>
+<div class="y_wrap">
+          <div class="y_pages">
+          <div class="y_page">
+          <div id="contentList">
+              <!--<div class="y_banner">
+                   test1
+              </div>-->
+              <div class="y_section">
+                    test2
+              </div>
+               <!-- end y_section -->
+              <div class="y_section">
+                  <div class="y_suBcon">
+                      <div class="y_tit01">
+                            test3
+                      </div>
+                      <div class="y_con001">
+                             test4
+                      </div>
+                      <div class="y_con01">
+                           test5
+                      </div>
+                      <div class="y_Xg">
+                          test6
+                      </div>
+                      <ul class="y_list_news">
+                          <li>
+                              <div class="m1">test7</div>
+                              <div class="m2">test8</div>
+                          </li>
+                      </ul>
+                  </div>
+              </div>
+              <!-- end y_section -->
+          </div>
+          <!-- end contentList-->
+          </div>
+
+          </div>
 </div>
