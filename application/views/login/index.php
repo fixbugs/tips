6cced714d54bf62f2d13eac21018a7055a30d94b
fuxin_home <goitt0519@gmail.com>
2016-06-09 22:38:54 +0800

    template file change for login

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -1,5 +1,5 @@
 
-<form action='/index.php/login/index' method='post'>
+<form name="post-form" action='/index.php/login/index' method='post'>
     <div class="form-group">
             <label for="username">用户名：</label>
             <input type="text" class="form-control" id="username" placeholder="User Name" name='username'>
@@ -12,3 +12,39 @@
             <button type="reset" class="btn btn-default">Reset</button>
 </form>
 
+<script>
+          $(document).ready(function(){
+              console.log( $("form").serialize() );
+          });
+$("button[type=submit]").click(function(e){
+    if(e && e.preventDefault){
+        e.preventDefault();
+    }else{
+        window.event.returnValue = false;
+    }
+    url =  $("form[name='post-form']")[0].action;
+    if(!url){
+        alert('action is null');
+        return;
+    }
+    serialize_data = $("form[name='post-form']").serialize();
+    $.ajax({
+     type: 'post',
+     url: url,
+     data: serialize_data,
+     dataType: "json",
+     success: function(data) {
+         if(data.status){
+             if(data.redirect){
+                 location.href=data.redirect;
+                 return;
+             }else{
+                 alert(data.message);
+             }
+         }else{
+             alert(data.message);
+         }
+     }
+     });
+});
+</script>
