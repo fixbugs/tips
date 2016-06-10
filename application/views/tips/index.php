ea43c754bcd541bddf9ed407e86b3cad805ea295
fuxin_home <goitt0519@gmail.com>
2016-06-11 07:34:45 +0800

    add fix tips index

--- application/views/tips/index.php
+++ application/views/tips/index.php
@@ -1,16 +1,45 @@
-<div>
+
+<table class="table table-striped">
+<thead>
+    <tr>
+        <th>ID</th>
+        <th>父ID</th>
+        <th>提示内容</th>
+        <th>编辑者</th>
+        <th>创建时间</th>
+        <th>编辑时间</th>
+        <th>状态</th>
+    </tr>
+</thead>
+<tbody>
 <?php foreach($tips as $tip){ ?>
-<ul>
-<li><?php echo $tip['tips_id'];?></li>
-<li><?php echo $tip['parent_id'];?></li>
-<li><?php echo $tip['tips_message'];?></li>
-<li><?php echo $tip['user_id'];?></li>
-<li><?php echo convert_time_to_zh($tip['create_time']);?></li>
-<li><?php echo $tip['edit_time'];?></li>
-<li><?php echo $tip['status'];?></li>
-</ul>
+<tr>
+    <td>
+    <?php echo $tip['tips_id'];?>
+    </td>
+    <td>
+    <?php echo $tip['parent_id'];?>
+    </td>
+    <td>
+    <?php echo $tip['tips_message'];?>
+    </td>
+    <td>
+    <?php echo $tip['user_id'];?>
+    </td>
+    <td>
+    <?php echo convert_time_to_zh($tip['create_time']);?>
+    </td>
+    <td>
+    <?php echo $tip['edit_time'];?>
+    </td>
+    <td>
+    <?php echo $tip['status'];?>
+    </td>
+</tr>
 <?php } ?>
-</div>
+</tbody>
+</table>
+
 <div>
 <a href = '/index.php/tips/add'>添加</a>
 <a href = ''>删除</a>
