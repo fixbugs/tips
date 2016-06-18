41b423897b4d2383b120278fc2571c51d0fc6eb4
fuxin_home <goitt0519@gmail.com>
2016-06-18 22:28:49 +0800

    user index fix

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -1,16 +1,62 @@
-<div>
+<div class="container">
+<div class="row">
+          <div class="col-sm-9">
+          <a class="btn btn-default" href="/index.php/user/add" role="button">添加</a>
+          <a class="btn btn-default" href="#" role="button">删除</a>
+          </div>
+</div>
+
+<table class="table table-striped">
+<thead>
+    <tr>
+        <th>ID</th>
+        <th>用户名</th>
+        <th>E-mail</th>
+        <th>创建时间</th>
+        <th>更新时间</th>
+        <th>操作</th>
+    </tr>
+</thead>
+<tbody>
 <?php foreach($users as $user){ ?>
-<ul>
-<li><?php echo $user['user_id'];?></li>
-<li><?php echo $user['password'];?></li>
-<li><?php echo $user['truename'];?></li>
-<li><?php echo $user['email'];?></li>
-<li><?php echo convert_time_to_zh($user['create_time']);?></li>
-<li><?php echo convert_time_to_zh($user['update_time']);?></li>
-</ul>
+<tr>
+    <td>
+    <?php echo $user['user_id'];?>
+    </td>
+    <td>
+    <?php echo $user['truename'];?>
+    </td>
+    <td>
+    <?php echo $user['email'];?>
+    </td>
+    <td>
+    <?php echo convert_time_to_zh($user['create_time']);?>
+    </td>
+    <td>
+    <?php
+if($user['update_time']){
+echo convert_time_to_zh($user['update_time']);
+}else{
+echo '';
+}
+?>
+    </td>
+    <td>
+<a href="/index.php/user/edit?id=<?php echo $user['user_id'];?>">编辑</a>
+    </td>
+</tr>
 <?php } ?>
-</div>
-<div>
-<a href = '/index.php/user/add'>添加</a>
-<a href = ''>删除</a>
-</div>
+</tbody>
+<thead>
+    <tr>
+        <th>ID</th>
+        <th>用户名</th>
+        <th>E-mail</th>
+        <th>创建时间</th>
+        <th>更新时间</th>
+        <th>操作</th>
+    </tr>
+ </thead>
+</table>
+
+
