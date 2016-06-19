c1f034dd6486c3341e9a07bcea97316c8df86686
fuxin_home <goitt0519@gmail.com>
2016-06-19 22:22:35 +0800

    add common js for from post

---
+++ static/js/common.js
@@ -0,0 +1,31 @@
+$("button[type=submit]").click(function(e){
+    if(e && e.preventDefault){
+        e.preventDefault();
+    }else{
+        window.event.returnValue = false;
+    }
+    url =  $("form[name='post-form']")[0].action;
+    if(!url){
+        alert('action is null');
+        return;
+    }
+    serialize_data = $("form[name='post-form']").serialize();
+    $.ajax({
+        type: 'post',
+        url: url,
+        data: serialize_data,
+        dataType: "json",
+        success: function(data) {
+            if(data.status){
+                if(data.redirect){
+                    location.href=data.redirect;
+                    return;
+                }else{
+                    alert(data.message);
+                }
+            }else{
+                alert(data.message);
+            }
+        }
+    });
+});
