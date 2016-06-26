87a810e68028f5f2fbf4cb1aa60e124d1200949b
fuxin_home <goitt0519@gmail.com>
2016-06-26 21:04:32 +0800

    add some features todo list for tips

--- application/views/features.php
+++ application/views/features.php
@@ -1,7 +1,9 @@
 <div class="contain">
       <div class="row">
           <div class="col-xs-12">
-              <p class="text-center">Just Features</p>
+              <p class="text-center">1.Fix all network equiments!</p>
+              <p class="text-center">2.Add user pic show.</p>
+              <p class="text-center">3.Add log anlysis for tips.</p>
           </div>
       </div>
 </div>
