6492086a5172c47d90c8c16e1c5cf3903fab2917
fuxin_home <goitt0519@gmail.com>
2016-07-05 21:55:35 +0800

    add tag add function

--- application/controllers/Tag.php
+++ application/controllers/Tag.php
@@ -29,4 +29,27 @@ class Tag extends GT_Controller {
             $this->render('tag/index', $data);
         }
     }
+
+    /**
+     * 添加标签方法
+     * @return mixed
+     */
+    public function add(){
+        if($this->isPost()){
+            $params = $this->isPost() ? $this->input->post():$this->input->get();
+            $ret = $this->tags_model->addTags($params);
+            if($ret){
+                $result = array_for_result(true, $this->tags_model->getModelError());
+            }else{
+                $result = array_for_result(false, $this->tags_model->getModelError());
+            }
+            $this->renderJson($result);
+        }else{
+            $params = $this->input->get();
+            $data['title'] = 'Tag Add';
+            $data['action'] = 'add';
+            $this->render('tag/add', $data);
+        }
+    }
+
 }
